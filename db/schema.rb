997e9dc170e7a90a00f4d4f408fa9650f1d62003
kaishu16 <barcelona_df_5@yahoo.com>
2020-01-04 04:22:32 +0000

    メッセージ通知

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_23_024311) do
+ActiveRecord::Schema.define(version: 2019_12_25_123057) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id"
@@ -55,20 +55,19 @@ ActiveRecord::Schema.define(version: 2019_11_23_024311) do
   end
 
   create_table "notifications", force: :cascade do |t|
-    t.integer "visited_id", null: false
+    t.integer "visited_id"
     t.integer "purchase_id"
     t.integer "comment_id"
     t.integer "review_id"
-    t.string "action", default: "", null: false
-    t.boolean "checked", default: false, null: false
+    t.string "action"
+    t.boolean "checked"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "visitor_id"
     t.integer "product_id"
     t.integer "favorite_id"
     t.integer "user_id"
-    t.index ["comment_id"], name: "index_notifications_on_comment_id"
-    t.index ["visited_id"], name: "index_notifications_on_visited_id"
+    t.integer "room_id"
   end
 
   create_table "posts", force: :cascade do |t|
